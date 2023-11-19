81b113fe815110a1e5a436cc99ea618444a3cac0
Amrit Sundarka <amritsundarka@gmail.com>
2023-11-19 18:30:17 +0530

    Final Try

--- Farming/Farming/views.py
+++ Farming/Farming/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-
-def homePage(request):
-    return render(request,"index.html")
 from django.http import JsonResponse
 import serial
 
-def arduino_data(request):
+def homePage(request):
     # Set the port to the one your Arduino is connected to
     port = 'COM4'  # Change this to your actual port (e.g., COM3 for Windows)
 
@@ -25,8 +22,8 @@ def arduino_data(request):
         # Print the received data
         print("Received data:", serial_data)
 
-        # Return the data as JSON response
-        return JsonResponse({'data': serial_data})
+        # Pass the data to the template
+        return render(request, "arduino/index.html", {'data': serial_data})
 
     except Exception as e:
         # Handle exceptions
