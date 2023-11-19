2c67f0f966f500c86df2db4631c0316d5a3537ee
Amrit Sundarka <amritsundarka@gmail.com>
2023-11-19 12:40:59 +0530

    CHanged view py and index

--- Farming/Farming/views.py
+++ Farming/Farming/views.py
@@ -1,26 +1,37 @@
 from django.shortcuts import render
-
-def homePage(request):
-    return render(request,"index.html")
 from django.http import JsonResponse
 import serial
+import serial.tools.list_ports
+
+# Function to find the Arduino port
+def find_arduino_port():
+    arduino_ports = [
+        p.device
+        for p in serial.tools.list_ports.comports()
+        if 'Arduino' in p.description
+    ]
+    if arduino_ports:
+        return arduino_ports[0]
+    else:
+        raise Exception("Arduino not found. Check the connection.")
+
+# Create a global serial object
+arduino_serial = None
+
+def homePage(request):
+    return render(request, "index.html")
 
 def arduino_data(request):
-    # Set the port to the one your Arduino is connected to
-    port = 'COM4'  # Change this to your actual port (e.g., COM3 for Windows)
-
-    # Set the baud rate to match the Arduino's baud rate
-    baud_rate = 9600
-
-    # Create a serial object
-    ser = serial.Serial(port, baud_rate, timeout=1)
+    global arduino_serial
 
     try:
-        # Read a line from the serial port
-        serial_data = ser.readline().decode('utf-8').strip()
+        # Initialize the serial port if not already open
+        if arduino_serial is None or not arduino_serial.is_open:
+            arduino_port = find_arduino_port()
+            arduino_serial = serial.Serial(arduino_port, 9600, timeout=1)
 
-        # Close the serial connection
-        ser.close()
+        # Read a line from the serial port
+        serial_data = arduino_serial.readline().decode('utf-8').strip()
 
         # Print the received data
         print("Received data:", serial_data)
@@ -28,6 +39,10 @@ def arduino_data(request):
         # Return the data as JSON response
         return JsonResponse({'data': serial_data})
 
+    except serial.SerialException as e:
+        # Handle serial communication issues
+        return JsonResponse({'error': f'Serial communication error: {str(e)}'})
+
     except Exception as e:
-        # Handle exceptions
+        # Handle other exceptions
         return JsonResponse({'error': str(e)})
